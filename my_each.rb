d69ab795e3019e5e62cd397bb3edd107fc2857d5
Travis Martin <travisemerymartin@gmail.com>
2019-03-04 17:43:35 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,15 @@
+empty_arg = Proc.new {puts "This block should not run!"}
+
 def my_each(array)
-  i=0
-  while i<array.length
-    yield array[i]
-    i+=1
+  if block_given?
+    i=0
+    while i<array.length
+      yield array[i]
+      i+=1
+    end
+  else
+    empty_arg.call
   end
 end
 
-my_each(array) {|thing| puts thing}
+my_each(array) {|item| puts item}
